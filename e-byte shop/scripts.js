dbe0af1c198a617c39bbc25e88d1828346ba52b0
BNSavov19 <57961506+BNSavov19@users.noreply.github.com>
2020-04-26 16:28:53 +0300

    New e-byte shop with monitors page

---
+++ e-byte shop/scripts.js
@@ -0,0 +1,4 @@
+const selectElement = (element) => document.querySelector(element);
+selectElement( element '.menu-icons'),addEventListener( type: 'click', listener: () => {
+    selectElement( element 'nav').classList.toggle( token: 'activate');
+});
