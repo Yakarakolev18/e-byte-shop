93ec23c11e0d29a9e74a9a0ba252155eae9671e0
Yakarakolev18 <yakarakolev18@codingburgas.bg>
2020-06-01 09:00:11 +0300

    Massive update

--- e-byte shop/scripts.js
+++
@@ -1,4 +0,0 @@
-const selectElement = (element) => document.querySelector(element);
-selectElement( element '.menu-icons'),addEventListener( type: 'click', listener: () => {
-    selectElement( element 'nav').classList.toggle( token: 'activate');
-});
